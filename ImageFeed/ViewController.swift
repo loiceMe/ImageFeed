e98596c1630704ca5ffb6f6b66c61560126e4b34
Dmitry Krivenko <d.n.krivenko@gmail.com>
2025-02-27 17:36:45 +0200

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by   Дмитрий Кривенко on 27.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
